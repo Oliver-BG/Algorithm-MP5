7245647130fe45823661a7947cb264ddbff83aa3
veinsss <47320119+lilheart@users.noreply.github.com>
2023-03-06 18:34:48 +0800

    updated main now w Guassian and DFS

--- Main.cpp
+++ Main.cpp
@@ -89,16 +89,79 @@ int dfs_driver()
     return 0;
 }
 
-int guassian_driver()
+void gauss_jordan(vector<vector<double>> &matrix)
 {
+    int n = matrix.size();
+    for (int i = 0; i < 2; i++) // modified loop
+    {
+        double pivot = matrix[i][i];
 
+        for (int k = i; k < n; k++)
+        {
+            matrix[i][k] /= pivot;
+        }
+
+        for (int j = 0; j < n; j++)
+        {
+            if (j != i)
+            {
+                double factor = matrix[j][i] / matrix[i][i];
+                for (int k = i + 1; k < n; k++)
+                {
+                    matrix[j][k] -= factor * matrix[i][k];
+                }
+                matrix[j][i] = 0;
+            }
+        }
+
+        cout << "Iteration " << i + 1 << ":\n";
+        for (const auto &row : matrix)
+        {
+            for (const auto &elem : row)
+            {
+                cout << setprecision(2) << elem << ' ';
+            }
+            cout << '\n';
+        }
+        cout << '\n';
+    }
+
+    cout << "Final matrix:\n";
+    for (const auto &row : matrix)
+    {
+        for (const auto &elem : row)
+        {
+            cout << elem << ' ';
+        }
+        cout << '\n';
+    }
 }
 
+int guassian_driver()
+{
+    vector<vector<double>> matrix(3, vector<double>(3));
+
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            cout << "Enter a" << i + 1 << j + 1 << ": "
+                 << "\n";
+            cin >> matrix[i][j];
+        }
+    }
+
+    gauss_jordan(matrix);
+}
 
 int main()
 {
     int x;
-    cout << "Enter an operation: ";
+    cout << "Enter an operation " << endl;
+    cout << " [1] DFS " << endl;
+    cout << " [2] BFS " << endl;
+    cout << " [3] Guassian Calculator " << endl;
+    cout << "INPUT : " ;
     cin >> x;
 
     switch (x)
@@ -111,7 +174,7 @@ int main()
         // BFS();
         break;
     case 3:
-        // Guassian():
+        guassian_driver();
         break;
     default:
         cout << "invalid input";
